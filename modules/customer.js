7c0596cfcd105e983b22d537f49db32240e2cce9
st0534 <estypwolak>
2024-08-05 22:08:53 +0300

    add a function - addCustomer

--- modules/customer.js
+++ modules/customer.js
@@ -1,7 +1,37 @@
 require('dotenv').config()
-
-
 const {v4} = require('uuid')
 const {MongoOperations} = require('../services/mongo-operations')
-const {errorTypes} = new MongoOperations(process.env.MONGO_CUSTOMER_DB)
+const mongoOperations  = new MongoOperations(process.env.MONGO_CUSTOMER_DB)
 
+const existCustomerName = async (customerName) => {
+    mongoOperations.Collection = process.env.MONGO_CUSTOMER_COLLECTION
+    try {
+        const response = await mongoOperations.find({ filter: { customerName: customerName } })
+        return response.length > 0
+    } catch (error) {
+        throw error
+    }
+}
+
+const createCustomer = async (customer) => {
+
+    if (await existCustomerName(customer.customerName)) {
+        const error = {
+            message: `customerName '${customer.customerName}' is not available`,
+            type: errorTypes.VALIDATION
+        }
+        throw error
+    }
+    const id = v4()
+    customer.id = id
+    try {
+        mongoOperations.Collection = process.env.MONGO_CUSTOMER_COLLECTION
+        const response = await mongoOperations.insertItem(customer)
+        return customer
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+module.exports = { createCustomer, existCustomerName };
